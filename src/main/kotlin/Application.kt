54acd61fd06a8cbc6c4768d2a66bb72fe713b108
peterkrglv <peterkrglv@gmail.com>
2025-04-04 00:19:31 +0300

    first commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,15 @@
+package ru.drujite
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
